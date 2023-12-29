09d2fc0d11fa4e147f1a06bfa596d5c87a856a0f
Rosel Sosa González <rsosag8801@gmail.com>
2023-12-29 12:54:40 -0500

    Add migration for user ref to properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,8 +1,10 @@
 class Property < ApplicationRecord
   belongs_to :community
+  belongs_to :user
 
   has_many_attached :images
 
   validates :price, numericality: { greater_than: 0 }
   validates :community_id, presence: true
+  validates :user_id, presence: true
 end
