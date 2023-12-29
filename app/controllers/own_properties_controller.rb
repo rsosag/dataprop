5eb0d61d6e5469df5196bcff372f714d81724ddb
Rosel Sosa González <rsosag8801@gmail.com>
2023-12-29 18:08:57 -0500

    Add action to destroy properties

--- app/controllers/own_properties_controller.rb
+++ app/controllers/own_properties_controller.rb
@@ -1,4 +1,6 @@
 class OwnPropertiesController < ApplicationController
+
+  before_action :set_own_property, only: %i[destroy]
   def index
     @own_properties = current_user.properties.includes(:community).page(params[:page])
   end
@@ -15,9 +17,18 @@ class OwnPropertiesController < ApplicationController
       redirect_to own_properties_path
     else
       flash[:danger] = t('record_create_fail')
-
       render :new
     end
+  end
+
+  def destroy
+    if @own_property.destroy
+      flash[:success] = t('record_destroy_success')
+    else
+      flash[:danger] = t('record_destroy_fail')
+    end
+
+    redirect_to own_properties_path
   end
 
   private
@@ -27,5 +38,9 @@ class OwnPropertiesController < ApplicationController
       [:operation_type, :price, :currency, :community_id, :address, :area, :number_of_rooms, :number_of_bathrooms,
        :description, { images: [] }]
     )
+  end
+
+  def set_own_property
+    @own_property = current_user.properties.find(params[:id])
   end
 end
